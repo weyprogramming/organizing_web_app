dfdf5773b1da7e0d8943461c01d26c031dea69f2
weyprogramming <weyrowitzlennart@gmail.com>
2022-06-02 15:32:18 +0200

    Updated Readme.md and changed some static javascript

--- README.md
+++ README.md
@@ -1,34 +1,24 @@
-# hompage
+# Organizing Web App
 
-## Project setup
-```
-npm install
-```
+## Current Status
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+By now, I have just coded some static HTML and CSS (Bootstrap) to get an overview on what to do. I also added some static Javascript-Objects to render the page based on them. I will start to write the backend as soon as I finished the frontend to a point on which I can say which models will be needed.
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## Requirements
 
-### Run your unit tests
-```
-npm run test:unit
-```
+To run the development version of this project, you used to have Node.js to be installed on your machine. 
 
-### Run your end-to-end tests
-```
-npm run test:e2e
-```
+Get Node.js from here:
+https://nodejs.org/en/download/
 
-### Lints and fixes files
-```
-npm run lint
-```
+On Windows, installing it with the .msi installer is the most easiest way.
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Start the current Development Build
+
+To run the developemt build of this project locally you have to do the following (fitting for Windows OS, not tested on other OS):
+
+1. Go to Explorer and navigate inside the cloned repository (Folder you have downloaded)
+2. In the navigation-section (URI-Adress-Line, right under the menu buttons), type in "cmd"
+3. In the opening Command-Line-Interface, type in "npm run serve"
+4. The local dev server ist starting now
+5. After the local build is finished, you can access the Vue.js web-app at "https://localhost:8080/
--- src/views/apps/IdeasDetails.vue
+++ src/views/apps/IdeasDetails.vue
@@ -1,72 +1,90 @@
 <template>
-    <div class="ideas">
-        <div class="d-flex justify-content-center">
-            <div class="col-10">
-                <div class="p-1 mb-4 bg-light rounded-3">
-                    <div class="container-fluid py-5">
-                    <h1 class="text-center display-6 fw-bold">Ideen / Brainstorming</h1>
-                    <p class="text-center fs-4">Threads mit Themen zu denen Vorschläge erarbeitet werden sollen</p>
-                    </div>
-                </div>
-                <div class="col mb-2">
-                    <div class="card-bordered">
-                        <div class="card-header bg-danger fw-bold fs-5">
-                            <div class="row align-items-center">
-                                <div class="col text-start">
-                                    {{thread.title}}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-body bg-light text-start">
-                            {{thread.description}}
-                        </div>
-                        <div class="card-footer bg-secondary text-white">
-                            <div class="row align-items-center">
-                                <div class="col text-start">
-                                    Von: {{thread.creator}}
-                                </div>
-                                <div class="col text-end">
-                                    {{thread.created_at}}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <h2 class="text-start mt-5">Kommentare</h2>
-                <div class="col mt-3">
-                   <div v-for="comment in thread.comments" :key="comment.id" class="card mb-2">
-                       <div class="card-body bg-light">
-                           <div class="container-fluid">
-                                <div class="row align-items-center">
-                                    <div class="col-lg-9 mb-4 mb-lg-0 text-start">
-                                        {{comment.text}}
-                                    </div>
-                                    <div class="col-lg-3 d-grid gap-2">
-                                        <div class="btn btn-success btn-sm">Upvotes: {{comment.upvotes}}</div>
-                                        <div class="btn btn-danger btn-sm">Downvotes: {{comment.downvotes}}</div>
-                                    </div>
-                                </div>
-                           </div>
-                       </div>
-                       <div class="card-footer bg-secondary text-white">
-                            <div class="row align-items-center">
-                                <div class="col text-start">
-                                    Von: {{comment.user}}
-                                </div>
-                                <div class="col text-end">
-                                    {{thread.created_at}}
-                                </div>
-                            </div>
-                        </div>
-                   </div>
-                </div>
-                <div class="input-group mb-3 mt-4">
-                    <textarea type="text" class="form-control" placeholder="Schreibe einen Kommentar..." aria-describedby="make-comment-button" rows="3"></textarea>
-                    <button class="btn btn-dark col-lg-3" type="button" id="make-comment-button">Senden</button>
-                </div>
-            </div>
+  <div class="ideas">
+    <div class="d-flex justify-content-center">
+      <div class="col-10">
+        <div class="p-1 mb-4 bg-light rounded-3">
+          <div class="container-fluid py-5">
+            <h1 class="text-center display-6 fw-bold">Ideen / Brainstorming</h1>
+            <p class="text-center fs-4">
+              Threads mit Themen zu denen Vorschläge erarbeitet werden sollen
+            </p>
+          </div>
         </div>
+        <div class="col mb-2">
+          <div class="card-bordered">
+            <div class="card-header bg-danger fw-bold fs-5">
+              <div class="row align-items-center">
+                <div class="col text-start">
+                  {{ thread.title }}
+                </div>
+              </div>
+            </div>
+            <div class="card-body bg-light text-start">
+              {{ thread.description }}
+            </div>
+            <div class="card-footer bg-secondary text-white">
+              <div class="row align-items-center">
+                <div class="col text-start">Von: {{ thread.creator }}</div>
+                <div class="col text-end">
+                  {{ thread.created_at }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h2 class="text-start mt-5">Kommentare</h2>
+        <div class="col mt-3">
+          <div
+            v-for="comment in thread.comments"
+            :key="comment.id"
+            class="card mb-2"
+          >
+            <div class="card-body bg-light">
+              <div class="container-fluid">
+                <div class="row align-items-center">
+                  <div class="col-lg-9 mb-4 mb-lg-0 text-start">
+                    {{ comment.text }}
+                  </div>
+                  <div class="col-lg-3 d-grid gap-2">
+                    <div class="btn btn-success btn-sm">
+                      Upvotes: {{ comment.upvotes }}
+                    </div>
+                    <div class="btn btn-danger btn-sm">
+                      Downvotes: {{ comment.downvotes }}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer bg-secondary text-white">
+              <div class="row align-items-center">
+                <div class="col text-start">Von: {{ comment.user }}</div>
+                <div class="col text-end">
+                  {{ thread.created_at }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3 mt-4">
+          <textarea
+            type="text"
+            class="form-control"
+            placeholder="Schreibe einen Kommentar..."
+            aria-describedby="make-comment-button"
+            rows="3"
+          ></textarea>
+          <button
+            class="btn btn-dark col-lg-3"
+            type="button"
+            id="make-comment-button"
+          >
+            Senden
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -92,7 +110,7 @@ export default {
           {
             id: 2,
             user: 'Jasper Ziegler',
-            text: 'Andre Hunko ist der beste Chef der Welt!',
+            text: 'Nee das finde ich doof!',
             created_at: '22.22.2022',
             upvotes: 5555,
             downvotes: -300
